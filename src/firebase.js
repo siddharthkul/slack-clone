15b40551562f79ddc030ae0d38944afeb0989c92
Siddharth Kulkarni <sid@nightingalesecurity.com>
2019-08-10 17:09:20 -0700

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
